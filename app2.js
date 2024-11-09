e72d22ec5eef2b48699ea96623e21bf5d540b106
Valerie Á <valerie.alvarez@utp.edu.co>
2024-11-09 12:28:56 -0500

    -New layout -mobile-first responsive -popup for task creation

---
+++ app2.js
@@ -0,0 +1,9 @@
+const list = document.getElementById("listview");
+
+function openCreator(){
+    document.getElementById("createTask").style.display="block";
+}
+
+function closeCreator() {
+    document.getElementById("createTask").style.display = "none";
+  }
