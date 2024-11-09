34066a6c0357c6a6eb0f2c4286319d9a8fa7b470
Valerie Á <valerie.alvarez@utp.edu.co>
2024-11-09 12:30:32 -0500

    Deleted duplicated files

--- app.js
+++ app.js
@@ -1,38 +1,9 @@
+const list = document.getElementById("listview");
 
-const taskForm = document.getElementById('taskForm');
+function openCreator(){
+    document.getElementById("createTask").style.display="block";
+}
 
-const taskList = [];
-taskForm.addEventListener('submit', event =>{
-    event.preventDefault();
-    let nombre = event.target.name.value;
-    let description = event.target.description.value;
-    
-    const Task = {
-        nombre: nombre,
-        description: description,
-    }
-
-    taskList.push(Task)
-
-    const taskList_string = JSON.stringify(taskList);
-    localStorage.setItem('taskList', taskList_string);
-    taskForm.reset();
-
-    const taskList_temp = JSON.parse(localStorage.getItem('taskList'));
-    console.log("tarea recuperada: ", taskList_temp);
-    
-    const results = document.getElementById('results');
-
-    if(taskList_temp){
-      taskList_temp.forEach((task) => {
-        results.innerHTML += `
-        <div class="taskElement">
-        <h3>Task Name: ${ task.nombre }</h3>
-        <p>Description: ${ task.description }</p><br>
-        </article>
-        `
-      })
-    }
-} )
-
-
+function closeCreator() {
+    document.getElementById("createTask").style.display = "none";
+  }
--- app2.js
+++
@@ -1,9 +0,0 @@
-const list = document.getElementById("listview");
-
-function openCreator(){
-    document.getElementById("createTask").style.display="block";
-}
-
-function closeCreator() {
-    document.getElementById("createTask").style.display = "none";
-  }
