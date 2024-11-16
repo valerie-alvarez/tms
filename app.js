72029f6676f5b8ca469792a8f7e8873577af70d9
iMac <imac21@MacBook-Pro-de-iMac-2.local>
2024-11-16 17:16:29 -0500

    you have no tasks message appears when tasklist is empty

--- app.js
+++ app.js
@@ -1,33 +1,10 @@
 const taskForm = document.getElementById('creationForm');
 const taskList = [];
 
+/**Imprime lista de tareas*/
+function printList(List, htmlSpot){
 
-taskForm.addEventListener('submit', event => {
-    event.preventDefault();
-    listview.innerHTML='';
-    let nombre = event.target.name.value;
-    let details = {"Details" : [event.target.details.value],
-                    "Priority" : [event.target.priority.value],
-                    "Deadline" : [event.target.deadline.value]};
-                   
-    const Task = {
-        name: nombre,
-        details: details
-        /** priority: document.getElementsByName("priority").value,
-            deadline: document.getElementsByName("deadline").value,
-            finished: "False"*/
-    }
-
-    taskList.push(Task)
-
-    const taskList_string = JSON.stringify(taskList);
-    localStorage.setItem('taskList', taskList_string);
-    taskForm.reset();
-
-    const taskList_temp = JSON.parse(localStorage.getItem('taskList'));
-    console.log("tarea recuperada: ", taskList_temp);
-
-    taskList_temp.forEach((task,index) => {
+        List.forEach((task,index) => {
         const newli = document.createElement('li');
         newli.classList.add('taskTitle');
 
@@ -36,13 +13,39 @@ taskForm.addEventListener('submit', event => {
         </div>`;
         
         newli.innerHTML = ourhtml;
-        listview.appendChild(newli);
-        closeCreator();
+        htmlSpot.appendChild(newli);
+
+})}
+
+/**Crear tarea nueva*/
+taskForm.addEventListener('submit', event => {
+    event.preventDefault();
+    listview.innerHTML='';
+    let nombre = event.target.name.value;
+    let details = {"Details" : [event.target.details.value],
+                    "Priority" : [event.target.priority.value],
+                    "Deadline" : [event.target.deadline.value]
+    };     
+    const Task = {
+        name: nombre,
+        details: details
+    };
+
+    taskList.push(Task);
+    const taskList_string = JSON.stringify(taskList);
+    localStorage.setItem('taskList', taskList_string);
+    taskForm.reset();
+
+    const taskList_temp = JSON.parse(localStorage.getItem('taskList'));
+    console.log("tarea recuperada: ", taskList_temp);
+
+    printList(taskList_temp, listview);
+    closeCreator();
 
     });
-})
-        
 
+        
+/**Toggle de ventana de creación de tareas */
 function openCreator(){
     document.getElementById("createTask").style.display="flex";
     document.getElementById("seeTask").style.display="none";
@@ -63,6 +66,7 @@ function closeCreator() {
     deadline1.disabled = true;
 }
 
+/**Toggle para ver menu de detalles de tarea */
 function showDetails(name,Details,Priority,Deadline,index){
     document.getElementById("seeTask").style.display="flex";
     document.getElementById("createTask").style.display="none";
@@ -77,52 +81,56 @@ function showDetails(name,Details,Priority,Deadline,index){
     priority1.value =`${Priority}`
     deadline1.value =`${Deadline}`
 
+    /**Marcar tarea como finalizada */
     const finished = document.getElementById('finishTask');
     finished.onclick = function () {
         finishedTask(index);
     };
-
-    const taskList1 = [];
-
+    const finishedList = [];
+    /**Aquí define función */
     function finishedTask(index) {
 
         taskList.splice(index, 1);
 
         localStorage.setItem('taskList', JSON.stringify(taskList));
-
+        
         listview.innerHTML = '';
-        taskList.forEach((task, newIndex) => {
+
+        printList(taskList, listview);
+        
+        const isEmpty = JSON.parse(localStorage.getItem('taskList'));
+
+        if (!isEmpty || isEmpty.length == 0) {
             const newli = document.createElement('li');
             newli.classList.add('taskTitle');
-
+    
             const ourhtml = `
-            <div class="header" onclick ="showDetails('${task.name}','${task.details.Details}','${task.details.Priority}','${task.details.Deadline}',${newIndex})"> ${task.name}
-            </div>`;
+            <div class="header">you have no tasks</div>`;
             
             newli.innerHTML = ourhtml;
             listview.appendChild(newli);
-        });
-
+        }
 
         closeCreator();
                     
         let nombre1 = name1.value;
         const Task1 = {
             name: nombre1,
-            /** priority: document.getElementsByName("priority").value,
-                deadline: document.getElementsByName("deadline").value,
-                finished: "False"*/
         }
     
-        taskList1.push(Task1)
+        finishedList.push(Task1)
     
-        const taskList_string1 = JSON.stringify(taskList1);
-        localStorage.setItem('taskList1', taskList_string1);
+        const finishedList_string = JSON.stringify(finishedList);
+        localStorage.setItem('finishedList', finishedList_string);
     
-        const taskList_temp1 = JSON.parse(localStorage.getItem('taskList1'));
-        console.log("tarea recuperada: ", taskList_temp1);
-    
-        taskList_temp1.forEach((task,index) => {
+        const finishedList_temp = JSON.parse(localStorage.getItem('finishedList'));
+        console.log("finalizadas: ", finishedList_temp);
+        
+        
+        closeCreator();
+
+
+        finishedList_temp.forEach((task,index) => {
             const newli = document.createElement('li');
             newli.classList.add('finishedTask');
     
@@ -132,9 +140,8 @@ function showDetails(name,Details,Priority,Deadline,index){
             
             newli.innerHTML = ourhtml;
             doneList.appendChild(newli);
-            closeCreator();
-    
         });
+
     }
 
     const edit = document.getElementById('editInputs');
@@ -156,7 +163,7 @@ function showDetails(name,Details,Priority,Deadline,index){
                 Deadline: deadline1
             }
         };
-    
+        
         localStorage.setItem('taskList', JSON.stringify(taskList));
     
         listview.innerHTML = '';
@@ -171,13 +178,14 @@ function showDetails(name,Details,Priority,Deadline,index){
             newli.innerHTML = ourhtml;
             listview.appendChild(newli);
         });
-    
+        document.getElementById("editInputs").style.display="none"; 
         closeCreator();
     }   
 
 }
     
 function editTask(){
+    document.getElementById("editInputs").style.display="block"; 
     let name1 = document.getElementById("name1");
     let details1 = document.getElementById("details1");
     let priority1 = document.getElementById("priority1");
